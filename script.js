cb50abcf396eba7773f77443fe77f207914708b2
Axel <axel@MacBook-Pro-de-Axel.local>
2021-04-08 17:38:19 +0200

    add menu responsive

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+function menu() {
+  const btn = document.querySelector(".burger");
+  const menu = document.querySelector(".nav-links");
+  const li = document.querySelectorAll(".nav-links li");
+  const imgBurger = document.querySelector(".burger img");
+
+  btn.addEventListener("click", () => {
+    if (menu.classList.contains("active")) {
+      closeMenu();
+    } else {
+      openMenu();
+    }
+  });
+
+  li.forEach((li) => {
+    li.addEventListener("click", closeMenu);
+  });
+
+  function openMenu() {
+    menu.classList.add("active");
+    setTimeout(() => {
+      imgBurger.src = "images/icon-close.svg";
+    }, 400);
+  }
+
+  function closeMenu() {
+    menu.classList.remove("active");
+    setTimeout(() => {
+      imgBurger.src = "images/icon-hamburger.svg";
+    }, 400);
+  }
+}
+
+menu();
+
 $(".carousel").owlCarousel({
   margin: 20,
   loop: true,
